49fc315f7111dd5043ada06ba48d22bc13745fd9
Guozheng Ge <gzge@yahoo-inc.com>
2013-06-03 23:08:08 -0700

    added more components to my activity

--- src/codepath/demos/helloworlddemo/MyActivity.java
+++ src/codepath/demos/helloworlddemo/MyActivity.java
@@ -1,15 +1,34 @@
 package codepath.demos.helloworlddemo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MyActivity extends Activity {
-
+	List<String> names;
+	ArrayAdapter<String> adapter;
+	ListView lv;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my);
+		names = new ArrayList<String>();
+		names.add("Alex");
+		names.add("Claire");
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1,
+				names);
+		lv = (ListView)findViewById(R.id.name_list);
+		lv.setAdapter(adapter);
 	}
 
 	@Override
@@ -17,6 +36,18 @@ public class MyActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.my, menu);
 		return true;
+	}
+	
+	public void add(View v) {
+		EditText editTextName = (EditText)findViewById(R.id.editTextName);
+		adapter.add(editTextName.getText().toString());
+		editTextName.setText("");
+//		adapter.notifyDataSetChanged();
+	}
+	
+	public void reset(View v) {
+		adapter.clear();
+		adapter.notifyDataSetChanged();
 	}
 
 }
